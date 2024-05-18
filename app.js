4ac3f7de4b7b5dcc01134bb41e4aa4bd1facd312
sylvester wamaya <sylvesterwamaya@gmail.com>
2024-05-18 16:06:15 +0300

    Implement search

--- app.js
+++ app.js
@@ -2,15 +2,41 @@
 document.addEventListener('DOMContentLoaded', () => {
     const searchEl = document.getElementById('search-field');
     const statsEl = document.getElementById('stats');
+    const articlesEl = document.getElementById('articles');
+    let articlesArray = []
 
     searchEl.focus();
+
+    const appendArticles = (articles) => {
+        articles.forEach(element => {
+            const div1El = document.createElement('div');
+            const div2El = document.createElement('div');
+            const imageEl = document.createElement('img');
+            const h3El = document.createElement('h4');
+            const pEl = document.createElement('p');
+            const aEl = document.createElement('a');
+            imageEl.src = element.image;
+            h3El.innerText = element.title;
+            pEl.innerText = element.description;
+            aEl.href = element.url
+            aEl.innerText = "Read more";
+            div1El.classList.add('article');
+            div2El.appendChild(h3El);
+            div2El.appendChild(pEl);
+            div2El.appendChild(aEl);
+            div1El.appendChild(imageEl);
+            div1El.appendChild(div2El);
+            articlesEl.appendChild(div1El);
+                      
+
+        });
+    }
 
     setInterval(() => {
         fetch('http://127.0.0.1:3000/searches/search_stats')
             .then(response => response.json())
             .then(data => {
-                console.log(data);
-                
+              
                 data.forEach(element => {
                     const divEl = document.createElement('div');
                     const h3El = document.createElement('h4');
@@ -23,11 +49,21 @@ document.addEventListener('DOMContentLoaded', () => {
                     statsEl.appendChild(divEl);
                 });
             });
-    }, 10000000);
+
+            fetch('http://127.0.0.1:3000/articles')
+            .then(response => response.json())
+            .then(data => {
+                
+                articlesArray = data;
+                appendArticles(data);
+                
+               
+            });
+    }, 1000000);
    
     
     searchEl.addEventListener("input", () => {
-        const searchValue = searchEl.value;
+        let searchValue = searchEl.value;
         if (searchValue.length > 0){
             fetch("http://127.0.0.1:3000/searches", {
                 method: "POST",
@@ -36,8 +72,20 @@ document.addEventListener('DOMContentLoaded', () => {
                     'Content-Type': 'application/json'
                 }
             })
+            while (articlesEl.lastElementChild) {
+                articlesEl.removeChild(articlesEl.lastElementChild);
+              }
+            let filteredArticles= articlesArray.filter(article => article.title.toLowerCase().includes(searchValue.toLowerCase()) || article.description.toLowerCase().includes(searchValue.toLowerCase()));
+            console.log(filteredArticles)
+            appendArticles(filteredArticles);
+            
+
+
         }
+    
     });
+
+    
             
         }
     );
